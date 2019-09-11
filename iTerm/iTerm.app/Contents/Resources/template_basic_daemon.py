bd1ed4be9a72989c2765bae5adb1defa8d552cf9
XinJiangCN <jx240230@gmail.com>
2019-09-10 20:38:17 -0500

    update iterm

--- iTerm/iTerm.app/Contents/Resources/template_basic_daemon.py
+++ iTerm/iTerm.app/Contents/Resources/template_basic_daemon.py
@@ -1,31 +1,20 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.7
 
 import asyncio
 import iterm2
-import sys
 # This script was created with the "basic" environment which does not support adding dependencies
 # with pip.
 
-async def main(connection, argv):
-    app = await iterm2.async_get_app(connection)
-
-    # This is an example of a callback function. In this template, on_custom_esc is called when a
-    # custom escape sequence is received. You can send a custom escape sequence with this command:
+async def main(connection):
+    # This is an example of using an asyncio context manager to register a custom control
+    # sequence. You can send a custom control sequence by issuing this command in a
+    # terminal session in iTerm2 while this script is running:
     #
     # printf "\033]1337;Custom=id=%s:%s\a" "shared-secret" "create-window"
-    async def on_custom_esc(connection, notification):
-        print("Received a custom escape sequence")
-        if notification.sender_identity == "shared-secret":
-            if notification.payload == "create-window":
-                await app.async_create_window()
+    async with iterm2.CustomControlSequenceMonitor(connection, "shared-secret", r'^create-window$') as mon:
+        while True:
+            match = await mon.async_get()
+            await iterm2.Window.async_create(connection)
 
-    # Your program should register for notifications it wants to receive here. This example
-    # watches for custom escape sequences.
-    await iterm2.notifications.async_subscribe_to_custom_escape_sequence_notification(connection, on_custom_esc)
-
-    # Wait for messages indefinitely. This program will terminate when iTerm2 exits because
-    # dispatch_until_future will raise an exception when its connection closes.
-    await connection.async_dispatch_until_future(asyncio.Future())
-
-if __name__ == "__main__":
-    iterm2.Connection().run(main, sys.argv)
+# This instructs the script to run the "main" coroutine and to keep running even after it returns.
+iterm2.run_forever(main)
